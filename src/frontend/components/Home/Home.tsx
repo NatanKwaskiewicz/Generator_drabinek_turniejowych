31e4de0ca43bd1f52d3e52cb1f2f8c8c4f1bffb0
Natan <natan.kwaskiewicz@gmail.com>
2026-05-08 03:17:34 +0200

    rearrange home component to use carousel

--- src/frontend/components/Home/Home.tsx
+++ src/frontend/components/Home/Home.tsx
@@ -1,9 +1,29 @@
 import styles from './Home.module.scss'
+import Carousel from '../Carousel'
+import carouselData from '../../data/carouselData.ts'
+import { Link } from 'react-router'
+
 const Home = () => {
     return (
         <div className={styles.Home}>
-            <h1>Home</h1>
-            <p>Welcome to our bracket-generating site!!!</p>
+            <div className={styles.HomeTop}>
+                <div className={styles.HomeTopLeft}>
+                    <h1>The ultimate bracket tool!</h1>
+                    <p>
+                        Create bespoke tournament brackets with vast
+                        customization options right now!
+                    </p>
+                    <Link
+                        className={styles.HomeTopLeftLink}
+                        to="/bracketGenerator"
+                    >
+                        Create!
+                    </Link>
+                </div>
+                <div className={styles.HomeTopRight}>
+                    <Carousel data={carouselData} />
+                </div>
+            </div>
         </div>
     )
 }
