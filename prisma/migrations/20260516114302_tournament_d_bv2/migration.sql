109fe9bba85418622cd76d14df507d01e2357d38
Komputerr <bartosz.krol1602@gmail.com>
2026-05-16 15:00:22 +0200

    delete auto generated files

--- prisma/migrations/20260516114302_tournament_d_bv2/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `tournamentId` on the `participant` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `participant` DROP COLUMN `tournamentId`;
