ee7c6b58c9938c4b55ff534e571f1bf411be37ee
Komputerr <bartosz.krol1602@gmail.com>
2026-05-16 13:47:40 +0200

    add api routers

---
+++ src/backend/routes/participantsRouter.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import { getOneParticipant, getParticipants, postParticipant} from "../controllers/participantsController.ts"
+const router = Router();
+router.get('/', getParticipants);
+router.get('/:id', getOneParticipant);
+router.post('/', postParticipant);
+export default router;
