c7d1ba94364c717821296aeb164691c1ab2fe294
Komputerr <bartosz.krol1602@gmail.com>
2026-05-16 13:48:43 +0200

    connect routes to backend

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import app from './App.ts'
 const hostname: string = process.env.HOSTNAME ?? 'localhost'
-const port: number = Number(process.env.PORT)
+const port: number = Number(process.env.PORT) || 3000
 
 app.listen(port, hostname, () => {
     console.log(`Server started at http://${hostname}:${port}`)
