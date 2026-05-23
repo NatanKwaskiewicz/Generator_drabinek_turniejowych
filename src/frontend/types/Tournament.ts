034dcd40971cb9ba1bc932125f391bda544c7651
Natan <natan.kwaskiewicz@gmail.com>
2026-05-23 03:11:22 +0200

    add the Tournament type

---
+++ src/frontend/types/Tournament.ts
@@ -0,0 +1,23 @@
+export interface Tournament {
+    id: number
+    name: string
+    date: string
+    createdAt: string
+    formatId: number
+    format: { id: number; name: string }
+    TournamentTeam: {
+        teamId: number
+        team: { id: number; name: string }
+    }[]
+    Match: {
+        id: number
+        tournamentId: number
+        teamAId: number
+        teamBId: number
+        teamAScore: number
+        teamBScore: number
+        round: number
+        teamA: { id: number; name: string }
+        teamB: { id: number; name: string }
+    }[]
+}
