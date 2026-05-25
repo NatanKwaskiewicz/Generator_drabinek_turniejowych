7feab474e5a74bfc073add091deb7471520a5122
Natan <natan.kwaskiewicz@gmail.com>
2026-05-25 14:48:55 +0200

    add formatrouter to app

--- src/backend/App.ts
+++ src/backend/App.ts
@@ -4,6 +4,7 @@ import tournamentRouter from './routes/tournamentsRouter.ts'
 import teamMembersRouter from './routes/teamMembersRouter.ts'
 import teamRouter from './routes/teamsRouter.ts'
 import matchesRouter from './routes/matchesRouter.ts'
+import formatsRouter from './routes/formatsRouter.ts'
 
 const app = express()
 app.use(express.json())
@@ -18,5 +19,6 @@ app.use('/tournaments', tournamentRouter)
 app.use('/teammembers', teamMembersRouter)
 app.use('/teams', teamRouter)
 app.use('/matches', matchesRouter)
+app.use('/formats', formatsRouter)
 
 export default app
