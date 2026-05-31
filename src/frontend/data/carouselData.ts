5174c2bf9f36717ae9d61c4cd874727fda150d90
Natan <natan.kwaskiewicz@gmail.com>
2026-05-31 21:24:16 +0200

    delete unused images

--- src/frontend/data/carouselData.ts
+++ src/frontend/data/carouselData.ts
@@ -1,15 +1,9 @@
-// import img1 from '../assets/img1.jpg'
-// import img2 from '../assets/img2.jpg'
-// import img3 from '../assets/img3.jpg'
 import img4 from '../assets/img4.jpg'
 import img5 from '../assets/img5.jpg'
 import img6 from '../assets/img6.jpg'
 import type { Slide } from '../types'
 
 const carouselData: Slide[] = [
-    // { src: img1, alt: 'Carousel image 1' },
-    // { src: img2, alt: 'Carousel image 2' },
-    // { src: img3, alt: 'Carousel image 3' },
     { src: img4, alt: 'Carousel image 4' },
     { src: img5, alt: 'Carousel image 5' },
     { src: img6, alt: 'Carousel image 6' },
